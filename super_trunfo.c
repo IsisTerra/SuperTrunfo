6a464ebd01c85833f3217bfe12d8cd674efbdace
Isis Terra <isisterragandra@gmail.com>
2025-04-07 22:04:17 +0000

    Inclusão de Densidade populacional e PIB Per Capita

--- super_trunfo.c
+++ super_trunfo.c
@@ -2,15 +2,20 @@
 
 // Desafio Super Trunfo - Países
 
-int main() {
+int main()
+{
 
     char estado, estado2;
     char codigo[4], codigo2[4];
     char cidade[50], cidade2[50];
-    int populacao, populacao2, turismo, turismo2;
-    float area, area2, pib, pib2;
+    int populacao, populacao2;
+    int turismo, turismo2;
+    float area, area2;
+    double pib, pib2;
+    float densPop, densPop2;
+    double pibPerCapita, pibPerCapita2;
 
-    printf("Bem vindo ao Super Trunfo!\n");
+    printf("***Bem vindo ao Super Trunfo!***\n");
 
     // Informações da primeira carta:
     printf("Por favor, insira os dados da sua primeira carta como solicitado\n");
@@ -25,7 +30,7 @@ int main() {
     printf("A área da cidade em quilômetros quadrados: ");
     scanf("%f", &area);
     printf("O Produto Interno Bruto da cidade: ");
-    scanf("%f", &pib);
+    scanf("%lf", &pib);
     printf("A quantidade de pontos turísticos na cidade: ");
     scanf("%d", &turismo);
 
@@ -42,31 +47,45 @@ int main() {
     printf("A área da cidade em quilômetros quadrados: ");
     scanf("%f", &area2);
     printf("O Produto Interno Bruto da cidade: ");
-    scanf("%f", &pib2);
+    scanf("%lf", &pib2);
     printf("A quantidade de pontos turísticos na cidade: ");
-    scanf("%d \n", &turismo2); //Espaçar mais o conteúdo
+    scanf("%d", &turismo2);
+
+    /*Calcular a Densidade Populacional: Divida a população da cidade pela sua área. O resultado será a densidade populacional, que representa o número de habitantes por quilômetro quadrado. Armazene esse valor em uma variável do tipo float.*/
+
+    densPop = (float)populacao / area;
+    densPop2 = (float)populacao2 / area2;
+
+    /*Calcular o PIB per Capita: Divida o PIB da cidade pela sua população. O resultado será o PIB per capita, que indica a riqueza média por pessoa na cidade. Armazene esse valor em uma variável do tipo float.*/
+
+    pibPerCapita = (double)pib / populacao;
+    pibPerCapita2 = (double)pib2 / populacao2;
 
     // Impressão de informações das duas cartas:
 
-    //Carta 1:
-    printf("\n Carta 1:\n"); //Mais uma linha para separar o conteúdo
+    // Carta 1:
+    printf("\n Carta 1:\n"); // Mais uma linha para separar o conteúdo
     printf("Estado: %c\n", estado);
     printf("Código: %s\n", codigo);
     printf("Nome da cidade: %s\n", cidade);
     printf("População: %d\n", populacao);
-    printf("Área: %f\n", area);
-    printf("PIB: %f\n", pib);
+    printf("Área: %.3f\n", area);
+    printf("PIB: %.3f\n", pib);
     printf("Número de Pontos Turísticos: %d\n", turismo);
+    printf("Densidade Populacional: %.2f hab/km²\n", densPop);
+    printf("PIB Per Capita: %.2lf reais\n", pibPerCapita);
 
-    //Carta 2:
-    printf("\n Carta 2:\n"); //Mais uma linha para separar o conteúdo
+    // Carta 2:
+    printf("\n Carta 2:\n"); // Mais uma linha para separar o conteúdo
     printf("Estado: %c\n", estado2);
     printf("Código: %s\n", codigo2);
     printf("Nome da cidade: %s\n", cidade2);
     printf("População: %d\n", populacao2);
-    printf("Área: %f\n", area2);
-    printf("PIB: %f\n", pib2);
+    printf("Área: %.3f\n", area2);
+    printf("PIB: %.3lf\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", turismo2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densPop2);
+    printf("PIB Per Capita: %.2lf reais\n", pibPerCapita2);
 
     return 0;
 }
